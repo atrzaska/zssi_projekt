f5193aa982dc15c0009b29b33200934d5b34cce6
Andrzej Trzaska <atrzaska2@gmail.com>
2014-01-22 09:41:49 +0100

    init2

---
+++ src/FaceRecognition/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FaceRecognition
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadTestImage_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnLoadTrainImage_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnTrain_Click(object sender, EventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
